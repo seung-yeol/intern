2e5530b8872ab176202a75835f4d0038bf38c7a1
seung-yeol <oks153123@naver.com>
2019-07-31 19:19:38 +0900

    사용할 library gradle 추가

--- app/src/main/java/com/osy/intern/MainActivity.kt
+++ app/src/main/java/com/osy/intern/MainActivity.kt
@@ -2,11 +2,15 @@ package com.osy.intern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.osy.intern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val viewDataBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
